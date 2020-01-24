a6b540bcc72350f13d259ef6dfbafb4ef4b7829d
Suporte 03 <breno.luna_@hotmail.com>
2020-01-24 11:59:13 -0300

    oioioio

--- src/controller/ProdutoController.js
+++ src/controller/ProdutoController.js
@@ -7,7 +7,7 @@ module.exports = {
         const produtos = await Produto.find9();
         res.render('front/front', {
             products : produtos,
-            cartTotal : await getCartTotal(req.user.id)
+            cartTotal :  ''//await getCartTotal(req.user.id)
         });
     },
 
